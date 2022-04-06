51aa13eee8a4e916595c2e30e5734c3d668732fd
Joohyun Lee <leejoohyun_my@naver.com>
2022-04-06 23:17:25 +0900

    MockDAO

---
+++ FrontControllerP-step3/src/main/java/model/MockDAO.java
@@ -0,0 +1,54 @@
+package model;
+
+import java.util.ArrayList;
+
+// Mock : 가짜 , 테스트를 위해 모조 DAO를 작성 
+public class MockDAO {
+	private static MockDAO instance=new MockDAO();
+	private MockDAO() {}
+	public static MockDAO getInstance() {
+		return instance;
+	}
+	public String findCarByNo(String carNo) throws Exception{
+		String car=null;
+		if(carNo.equals("1234"))
+			car="캐스퍼 검정";
+		return car;
+	}
+	public void register(String carInfo) throws Exception{
+		System.out.println("db에 "+carInfo+" 차량정보 등록");
+	}
+	public ArrayList<String> findAllCarList() throws Exception{
+		ArrayList<String> list=new ArrayList<String>();
+		list.add("소나타");
+		list.add("아우디");
+		list.add("아반떼");
+		return list;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
