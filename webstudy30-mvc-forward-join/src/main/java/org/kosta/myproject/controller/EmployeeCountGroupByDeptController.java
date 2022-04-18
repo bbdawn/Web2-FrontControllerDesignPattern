1e93b048f6ca5b732fb5e8cf29ce175fd16c4f02
Joohyun Lee <leejoohyun_my@naver.com>
2022-04-18 23:07:01 +0900

    Employee Count GroupBy Dept controller

---
+++ webstudy30-mvc-forward-join/src/main/java/org/kosta/myproject/controller/EmployeeCountGroupByDeptController.java
@@ -0,0 +1,20 @@
+package org.kosta.myproject.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.kosta.myproject.model.EmployeeDAO;
+
+public class EmployeeCountGroupByDeptController implements Controller {
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		request.setAttribute("groupList",EmployeeDAO.getInstance().findEmpCountGroupByDept());
+		return "employee-count-dept.jsp";
+	}
+}
+
+
+
+
+
+
