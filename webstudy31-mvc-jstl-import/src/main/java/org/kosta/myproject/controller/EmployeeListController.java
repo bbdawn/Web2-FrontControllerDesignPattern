e6d8ba7b5985d0b0e9924ec9375940d5e99be4ba
Joohyun Lee <leejoohyun_my@naver.com>
2022-04-25 18:19:45 +0900

    EmployeeListController

---
+++ webstudy31-mvc-jstl-import/src/main/java/org/kosta/myproject/controller/EmployeeListController.java
@@ -0,0 +1,16 @@
+package org.kosta.myproject.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.kosta.myproject.model.EmployeeDAO;
+
+public class EmployeeListController implements Controller {
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		request.setAttribute("employeeList", EmployeeDAO.getInstance().findAllEmployeeList());
+		request.setAttribute("url", "emp/employee-list.jsp");
+		return "layout.jsp";
+	}
+}
